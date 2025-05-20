18121875d087964a515425d5406947835509704b
adarshem <adarshem@gmail.com>
2025-05-19 18:35:44 -0700

    Created app stack

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,20 +1,20 @@
 #!/opt/homebrew/opt/node/bin/node
 import * as cdk from 'aws-cdk-lib';
 import { CdkStarterStack } from '../lib/cdk-starter-stack';
+import { PhotosStack } from '../lib/PhotosStack';
+import { PhotoHandlerStack } from '../lib/PhotoHandlerStack';
+import { Aspects, Tag } from 'aws-cdk-lib';
+import { TaggerAspect } from './TaggerAspects';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const photosStack = new PhotosStack(app, 'PhotosStack');
+new PhotoHandlerStack(app, 'PhotoHandlerStack', {
+  targetBucketArn: photosStack.photosBucketArn
 });
+
+// Add a tag to all resources in the stack
+Aspects.of(app).add(new Tag('Project', 'CDK Starter'));
+
+const tagger = new TaggerAspect('level', 'Test');
+Aspects.of(app).add(tagger);
